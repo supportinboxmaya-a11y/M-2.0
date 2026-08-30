e38302b2bdff66bff403b4257a01f15d32d5bd6d
Maya Dev <dev@maya.ai>
2026-08-30 16:50:41 +0000

    Add Bubblewrap TWA project for Maya Voice APK build

---
+++ app/src/main/java/com/maya/assistant/DelegationService.java
@@ -0,0 +1,14 @@
+package com.maya.assistant;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
