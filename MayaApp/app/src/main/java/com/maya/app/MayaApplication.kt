a5e64fe374d8cd794d06f8a87d6b315d9dae55fb
Maya Dev <dev@maya.ai>
2026-08-31 04:25:06 +0000

    Fix: Use @JvmName on instance property to avoid platform declaration clash

--- MayaApp/app/src/main/java/com/maya/app/MayaApplication.kt
+++ MayaApp/app/src/main/java/com/maya/app/MayaApplication.kt
@@ -7,6 +7,8 @@ import com.maya.app.repository.DataRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import kotlin.jvm.JvmStatic
+import kotlin.jvm.JvmName
 
 class MayaApplication : Application() {
 
@@ -24,12 +26,10 @@ class MayaApplication : Application() {
     companion object {
         @Suppress("UNUSED_PARAMETER")
         @JvmStatic
+        @JvmName("getInstance")
         lateinit var instance: MayaApplication
             private set
 
-        @JvmStatic
-        fun getInstance(): MayaApplication = instance
-        
         @JvmStatic
         fun getContext(): Context = instance.applicationContext
     }
