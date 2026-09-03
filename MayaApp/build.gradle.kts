87023b5f0fc040a8c2161601c6d6f57236310688
Maya Dev <dev@maya.ai>
2026-09-03 08:39:15 +0000

    Fix: Use Kotlin 1.8.20 + Compose Compiler 1.4.7 + Compose 1.4.3 (BOM 2023.03.00)

--- MayaApp/build.gradle.kts
+++ MayaApp/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.android.application") version "8.1.4" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.20" apply false
 }
 
 tasks.register("clean", Delete::class) {
